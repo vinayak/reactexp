03240299cca6585cd3f9686d9a75af28663c1722
Vinayak Malavade <vinay.malavade@gmail.com>
2017-11-20 10:44:59 +0530

    initial commit

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Greetings from './Greetings';
+
+class App extends React.Component{
+  render(){
+    return (
+      <Greetings />
+    )
+  }
+}
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import webpack from 'webpack'
+
+export default {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, '/client/index.js')
+],
+  output: {
+    filename: 'bundle.js',
+    path: '/',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        loaders: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+}
